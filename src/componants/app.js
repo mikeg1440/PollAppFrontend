66fdbac157d01c1348283d358cfbefe5a146e3c4
Mike Gaudreau <gaudreaum01@gmail.com>
2020-01-14 15:33:33 -0500

    Add a index.js and app.js file to handle the start of our SPA

---
+++ src/componants/app.js
@@ -0,0 +1,7 @@
+class App{
+  constructor(){
+    this.polls = new Polls()
+    this.mainContent = document.querySelector('#content')
+    this.sideBar = document.querySelector('#leftcolumn')
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+const app = new App()
+
+function createPoll(){
+  console.log('creating new poll')
+  // let content = document.querySelector('#content')
+  app.mainContent.innerHTML = '<h1> Create a new Poll!'
+}
