492f3ae7cf668f2230b3c6d17a1838c1efff1b28
Mike Gaudreau <gaudreaum01@gmail.com>
2020-01-15 16:24:42 -0500

    Refactor the createPoll method to be inside the Poll class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 
 const app = new App()
-
-function createPoll(){
-  console.log('creating new poll')
-  // let content = document.querySelector('#content')
-  app.mainContent.innerHTML = '<h1> Create a new Poll!'
-}
