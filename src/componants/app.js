29b93d1f89a352312e3d621883dac69c32bc3274
Mike Gaudreau <gaudreaum01@gmail.com>
2020-01-15 16:28:58 -0500

    Add listeners to app class to handle click of landing page buttons

--- src/componants/app.js
+++ src/componants/app.js
@@ -1,7 +1,25 @@
 class App{
   constructor(){
-    this.polls = new Polls()
     this.mainContent = document.querySelector('#content')
-    this.sideBar = document.querySelector('#leftcolumn')
+    this.polls = new Polls()
+    this.setListeners()
   }
+
+  setListeners(){
+    let newPollBtn = document.querySelector('#createPollBtn')
+    let takePollBtn = document.querySelector('#takePollBtn')
+    let viewResultsBtn = document.querySelector('#viewResultsBtn')
+
+    newPollBtn.addEventListener('click', this.renderNewPollForm)
+
+    takePollBtn.addEventListener('click', this.polls.renderMainList())
+
+    viewResultsBtn.addEventListener('click', this.renderResultsList())
+  }
+
+  renderNewPollForm(){
+    this.newForm = new PollForm()
+  }
+
+
 }
