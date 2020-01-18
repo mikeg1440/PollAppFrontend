35d885538f60b68fe725379aeec27eecfcd12783
Mike Gaudreau <gaudreaum01@gmail.com>
2020-01-17 21:20:59 -0500

    Refactor the code for the left arrow and add text

--- src/componants/app.js
+++ src/componants/app.js
@@ -21,8 +21,8 @@ class App{
 
     takePollBtn.addEventListener('mouseup', (e) => {
       this.mainContent.innerHTML = `
-      <h1>Select a Poll from the list on the side!</h1>
-      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRylMe2yXoy3YMook1-r2pxW0HFUjwyCSxnkgRAgGwhJIOuVrjP&s' alt='Arrow pointing left'/>
+      <h2>Select a Poll from the list on the side!</h2>
+      <img src='img/left-arrow.png' alt='Arrow pointing left'/>
       `
     })
 
