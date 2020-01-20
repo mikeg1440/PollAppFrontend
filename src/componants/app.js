aa9587f0c99671e1f969c0d51c1e70944e1a038b
Mike Gaudreau <gaudreaum01@gmail.com>
2020-01-19 20:51:53 -0500

    Refactor app class so method names are more logical and remove uneeded code

--- src/componants/app.js
+++ src/componants/app.js
@@ -23,27 +23,23 @@ class App{
 
     takePollBtn.addEventListener('mouseup', (e) => {
       app.leftColumn.parentNode.classList.add('expand')
-      this.setTestListener()
+      this.setCompressListListener()
     })
 
   }
 
-  expandPollList(e){
-    app.leftColumn.parentNode.classList.add('expand')
-    this.setTestListener()
-    document.removeEventListener('mouse')
-  }
+  // Compresses the left menu list of polls to transition back to original width and remove compress list listener
   compressPollList(e){
+    if (e.target.className == 'pollLink'){
+      let poll = app.pollsHandler.all.find(poll => poll.id == e.target.dataset.pollid)
+      poll.renderPoll()
+    }
     app.leftColumn.parentNode.classList.remove('expand')
-    document.removeEventListener('mousedown', app.listeners.pollListShrink, false)
+    document.removeEventListener('mousedown', app.listeners.pollListShrink, true)
   }
 
-  setTestListener(){
-    app.listeners.pollListShrink = document.addEventListener('mousedown', this.compressPollList, false)
-  }
-
-  removeListener(listenerVar){
-    document.removeEventListener('mousedown', listenerVar)
+  setCompressListListener(){
+    app.listeners.pollListShrink = document.addEventListener('mousedown', this.compressPollList, true)
   }
 
   renderNewPollForm(){
